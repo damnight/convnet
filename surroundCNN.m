629fd7fbc0a1bf8ec7069a031687fb4695d2f081
Marco <marco.ast@uni-ulm.de>
2017-06-22 07:53:53 +0200

    built gaborfilters

--- surroundCNN.m
+++ surroundCNN.m
@@ -27,12 +27,21 @@ cnn.layers = {
     struct('type', 'c', 'outputmaps', 6, 'kernelsize', 5) %convolution layer
 };
 
-%define gaborfilter
-wavelength = 0.56;%sigma/lambda = 0.56 from paper p51
-orientation = [0 30 60 90 120 150 180 210 240 270 300 330] %Number of Orientations used in paper = 12 from p53
-phaseOffset = [0 pi/2]
-gOdd = (wavelength, orientation, phaseOffset[0])
-gEven = (wavelength, orientation, phaseOffset[1])
+%define gaborfilter gb=gabor_fn(bw,gamma,psi,lambda,theta)
+% bw    = bandwidth, 
+% gamma = aspect ratio,
+% psi   = phase shift, 
+% lambda= wave length,
+% theta = angle in rad, 
+ 
+stdDeviation = 6;%p54 this is for coarse scale, =1.5 for fine scale
+aspectratio = 0.5; %paper p51
+bandwidth = 0.56; %sigma/lambda = 0.56 from paper p51
+orientation = [0 30 60 90 120 150 180 210 240 270 300 330]; %Number of Orientations used in paper = 12 from p53
+wavelength = stdDeviation/0.56; %p51
+phaseOffset = [0 pi/2];
+gOdd = gabor_fn(bandwidth,aspectratio, phaseOffset[0],wavelength ,orientation);
+gEven = gabor_fn(bandwidth,aspectratio, phaseOffset[1],wavelength ,orientation);
 
 %setup cnn
 cnn = cnnsetup(cnn, natFiles, train_y);
